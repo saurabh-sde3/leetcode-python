87ee2264e5922c0334c9a60a9b1a772203eaedd2
Saurabh Verma <saurabh.sde3@gmail.com>
2025-08-11 14:42:48 +0530

    2438. Range Product Queries of Powers

---
+++ 2438 Range Product Queries of Powers.py
@@ -0,0 +1,27 @@
+## 2438. Range Product Queries of Powers
+
+class Solution:
+    def productQueries(self, n, queries):
+        mod = 10**9 + 7
+
+        bins, rep = [], 1
+        while n > 0:
+            if n % 2 == 1:
+                bins.append(rep)
+            n //= 2
+            rep *= 2
+
+        m = len(bins)
+        results = [[0] * m for _ in range(m)]
+        for i in range(m):
+            cur = 1
+            for j in range(i, m):
+                cur = cur * bins[j] % mod
+                results[i][j] = cur
+
+        ans = []
+        for left, right in queries:
+            ans.append(results[left][right])
+        return ans
+    
+print(Solution().productQueries(15, [[0,1],[2,2],[0,3]]))
